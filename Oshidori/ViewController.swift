67c3b3f9815a0b3ee2837eea59e1c66110fe73dc
yamamoto tatsuya <52.wadachi.96@gmail.com>
2019-03-29 11:12:33 +0900

    initial commit

---
+++ Oshidori/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Oshidori
+//
+//  Created by 山本竜也 on 2019/3/29.
+//  Copyright © 2019 山本竜也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
