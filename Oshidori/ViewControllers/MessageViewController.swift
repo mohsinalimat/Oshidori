11dd79e51adb4e09be6181ca17eee648fbf230b2
yamamoto tatsuya <52.wadachi.96@gmail.com>
2019-04-08 16:43:22 +0900

    firebaseと連携し、contentを表示することができた！dateはまだできない。

--- Oshidori/ViewControllers/MessageViewController.swift
+++ Oshidori/ViewControllers/MessageViewController.swift
@@ -7,13 +7,16 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseFirestore
+import MessageKit
 
 class MessageViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    let messages:[(content:String, date:String)] = [(content:"ありがと", date : "2019/10/28"), (content:"content2", date: "2019/10/04")]
+    // let messages:[(content:String, date:String)] = [(content:"ありがと", date : "2019/10/28"), (content:"content2", date: "2019/10/04")]
+    var  messages:[(content:String, sendDate:String)] = []
     
     @IBOutlet weak var receiveTableView: UITableView!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,13 +28,46 @@ class MessageViewController: UIViewController, UITableViewDataSource, UITableVie
         
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        // firestoreからデータを持ってくる
+        let collectionRef = getColletionRef()
+        collectionRef.getDocuments() { (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
+            } else {
+                for document in querySnapshot!.documents {
+                    print("\(document.documentID) => \(document.data())")
+                    if let content = document.get("content"){
+                        if let date = document.get("created"){
+                            print(date) // -> FIRTimestamp: seconds=1554548323 nanoseconds=622850000>
+                            // TODO: FIRTimestamp -> String へのコンバートができていない。
+                            //self.messages.append((content: content as! String, sendDate: self.convertTimeStampToString(timestampDate: date as! TimeInterval)))
+                            self.messages.append((content: content as! String, sendDate: "2019/10/28"))
+                            
+                        }
+                    }
+                }
+                // firebaseにアクセスするよりも、tableViewのメソッドの方が先に走る。非同期通信だから。→リロードしてデータを反映させる。
+                self.receiveTableView.reloadData()
+            }
+        }
     }
-
+    
+    // TODO: convertができるようにする
+//        func convertTimeStampToString(timestampDate: TimeInterval) -> String {
+//            let date = NSDate(timeIntervalSince1970: timestampDate / 1000)
+//            let dateFormatter = DateFormatter()
+//            dateFormatter.dateFormat = "dd-MM-yyyy HH:mm:ss"
+//            let stringDate = dateFormatter.string(from: date as Date)
+//            return stringDate
+//        }
+    
+    
     // MARK: - Table view data source
-
+    
     func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
@@ -46,68 +82,81 @@ class MessageViewController: UIViewController, UITableViewDataSource, UITableVie
         // as! ReceiveMessageTableViewCell をつけないと、ReceiveMessageTableViewCell.swiftのパーツをいじることができない。
         let cell = tableView.dequeueReusableCell(withIdentifier: "receiveMesseageCell", for: indexPath) as! ReceiveMessageTableViewCell
         cell.contentLabel.text = messages[indexPath.row].content
-        cell.dateLabel.text = messages[indexPath.row].date
-
+        cell.dateLabel.text = messages[indexPath.row].sendDate
+        
         return cell
     }
     
+    // firebase 関連
+    private let db = Firestore.firestore()
+    private var reference: CollectionReference?
+    private let storage = Storage.storage().reference()
+    private func getColletionRef() -> CollectionReference {
+        guard let uid = User.shared.getUid() else {
+            fatalError("Uidを取得できませんでした。")
+        }
+        return db.collection("users").document(uid).collection("messages")
+    }
     
-
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        // #warning Incomplete implementation, return the number of rows
-//        return test.count
-//    }
-//
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-//        cell.textLabel?.text = test[indexPath.row]
-//        return cell
-//    }
-
+    
+    
+    
+    
+    //    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    //        // #warning Incomplete implementation, return the number of rows
+    //        return test.count
+    //    }
+    //
+    //
+    //    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    //        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    //        cell.textLabel?.text = test[indexPath.row]
+    //        return cell
+    //    }
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
