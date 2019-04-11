70a4cea3ea166388b13ac979fe9de9fa863df069
yamamoto tatsuya <52.wadachi.96@gmail.com>
2019-04-11 10:57:49 +0900

    add タイムラインの表示が可能になりました！！！歓喜！！！

--- Oshidori/ViewControllers/MessageViewController.swift
+++ Oshidori/ViewControllers/MessageViewController.swift
@@ -9,7 +9,6 @@
 import UIKit
 import Firebase
 import FirebaseFirestore
-import MessageKit
 
 class MessageViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
@@ -33,12 +32,7 @@ class MessageViewController: UIViewController, UITableViewDataSource, UITableVie
         getMessageDataFromFirestore_createTableView()
     }
 
-    func convertDateToString(timestampDate: NSDate) -> String {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-        let stringDate = dateFormatter.string(from: timestampDate as Date)
-        return stringDate
-    }
+    
     
     // MARK: - Table view data source
     
@@ -88,62 +82,5 @@ class MessageViewController: UIViewController, UITableViewDataSource, UITableVie
             self.receiveTableView.reloadData()
         }
     }
-    
-    //    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    //        // #warning Incomplete implementation, return the number of rows
-    //        return test.count
-    //    }
-    //
-    //
-    //    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    //        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-    //        cell.textLabel?.text = test[indexPath.row]
-    //        return cell
-    //    }
-    
-    /*
-     // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
-    
-    /*
-     // Override to support editing the table view.
-     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-     if editingStyle == .delete {
-     // Delete the row from the data source
-     tableView.deleteRows(at: [indexPath], with: .fade)
-     } else if editingStyle == .insert {
-     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-     }
-     }
-     */
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
     
 }
