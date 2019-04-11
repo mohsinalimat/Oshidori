72440254eea1f418040ad42ce545bd35630f0cc6
yamamoto tatsuya <52.wadachi.96@gmail.com>
2019-04-11 09:31:27 +0900

    timelineMessageのファイルを作成するところまで終了！

---
+++ Oshidori/Cell/TimelineMessageTableViewCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TimelineMessageTableViewCellTableViewCell.swift
+//  Oshidori
+//
+//  Created by 山本竜也 on 2019/4/11.
+//  Copyright © 2019 山本竜也. All rights reserved.
+//
+
+import UIKit
+
+class TimelineMessageTableViewCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Oshidori/ViewControllers/TimelineViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TimelineViewController.swift
+//  Oshidori
+//
+//  Created by 山本竜也 on 2019/4/11.
+//  Copyright © 2019 山本竜也. All rights reserved.
+//
+
+import UIKit
+
+class TimelineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
