9449a08aad5794ac4ccef9e2b956a8f4c2ae85d6
yamamoto tatsuya <52.wadachi.96@gmail.com>
2019-04-29 14:15:37 +0900

    マイページに必要なcellを作成

---
+++ Oshidori/Mypage Page/Cell/MessageReportTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageReportTableViewCell.swift
+//  Oshidori
+//
+//  Created by 山本竜也 on 2019/4/29.
+//  Copyright © 2019 山本竜也. All rights reserved.
+//
+
+import UIKit
+
+class MessageReportTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Oshidori/Mypage Page/MyImageAndNameTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  MyImageAndNameTableViewCell.swift
+//  Oshidori
+//
+//  Created by 山本竜也 on 2019/4/29.
+//  Copyright © 2019 山本竜也. All rights reserved.
+//
+
+import UIKit
+
+class MyImageAndNameTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+    
+}
