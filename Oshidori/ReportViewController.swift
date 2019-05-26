3123a2807bdf89e06b19795b3266a5d5d7e4d5fa
yamamoto tatsuya <52.wadachi.96@gmail.com>
2019-05-26 18:23:32 +0900

    移動した。

--- Oshidori/ReportViewController.swift
+++ Oshidori/ReportViewController.swift
@@ -23,13 +23,18 @@ class ReportViewController: UIViewController {
     }
     
     func setup() {
+        // TODO: たぶん正攻法じゃないから要変更
         
         let navigationView = UIView()
         self.view.addSubview(navigationView)
         
         let navBar = UINavigationBar()
         navBar.barTintColor = OshidoriColor.dark
+        navigationView.backgroundColor = OshidoriColor.dark
         
+        navBar.tintColor = OshidoriColor.background
+        navBar.titleTextAttributes = [.foregroundColor: OshidoriColor.background]
+
         navBar.frame.origin = self.view.safeAreaLayoutGuide.layoutFrame.origin
         navBar.frame.size = CGSize(width: self.view.safeAreaLayoutGuide.layoutFrame.width, height: 44)
         let navItem : UINavigationItem = UINavigationItem(title: "問題を報告する")
@@ -39,15 +44,9 @@ class ReportViewController: UIViewController {
         navBar.pushItem(navItem, animated: true)
         self.view.addSubview(navBar)
         
-        
-        //ここで背景変えてくれ
-        navigationView.backgroundColor = OshidoriColor.dark
         //navigationBarの背景のサイズと位置を調整
         navigationView.frame.origin = self.view.safeAreaLayoutGuide.owningView!.frame.origin
         navigationView.frame.size = CGSize(width: self.view.safeAreaLayoutGuide.owningView!.frame.width, height: navBar.frame.origin.y + navBar.frame.height)
-        
-        navBar.tintColor = OshidoriColor.background
-        navBar.titleTextAttributes = [.foregroundColor: OshidoriColor.background]
         
     }
     
