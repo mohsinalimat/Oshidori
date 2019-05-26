d916281407a2d42adbdab4af01f4d206f557de61
yamamoto tatsuya <52.wadachi.96@gmail.com>
2019-05-26 13:33:35 +0900

    CheckBoxのボタンのアイコンと実装を行なった

---
+++ Oshidori/Common/Extension/CheckBox.swift
@@ -0,0 +1,37 @@
+//
+//  CheckBox.swift
+//  Oshidori
+//
+//  Created by Tatsuya Yamamoto on 2019/5/26.
+//  Copyright © 2019 山本竜也. All rights reserved.
+//
+
+import UIKit
+
+class CheckBox: UIButton {
+    // Images
+    let checkedImage = UIImage(named: "Checkbox_afterSelected")! as UIImage
+    let uncheckedImage = UIImage(named: "Checkbox_beforeSelect")! as UIImage
+    
+    // Bool property
+    var isChecked: Bool = false {
+        didSet{
+            if isChecked == true {
+                self.setImage(checkedImage, for: UIControl.State.normal)
+            } else {
+                self.setImage(uncheckedImage, for: UIControl.State.normal)
+            }
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.addTarget(self, action:#selector(buttonClicked(sender:)), for: UIControl.Event.touchUpInside)
+        self.isChecked = false
+    }
+    
+    @objc func buttonClicked(sender: UIButton) {
+        if sender == self {
+            isChecked = !isChecked
+        }
+    }
+}
